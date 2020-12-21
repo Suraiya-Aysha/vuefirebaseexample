a553ad2b60b11aae06072f48758481c5b3b4b2a1
suraiya-aysha <sm.asa15nov@gmail.com>
2020-12-21 09:02:44 +0600

    vuefire plugin add

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import Vue from 'vue'
+// import VueFire from 'vuefire'
+import { firestorePlugin } from 'vuefire'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+// Vue.use(VueFire)
+Vue.use(firestorePlugin)
 
 Vue.config.productionTip = false
 
